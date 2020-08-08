2fc832efcc113d49aaf65d5dc86f32317312b572
sshipra319 <56846583+sshipra319@users.noreply.github.com>
2020-08-08 16:20:36 -0500

    HW2

--- q1.py
+++ q1.py
@@ -1,105 +1,156 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb  28 18:29:50 2020
+Created on Sat Mar 28 13:48:02 2020
 
 @author: Shipra
 """
 import numpy as np
-import copy
 import matplotlib.pyplot as plt
 
-N = 500
+dim = 0
+h = [0.1, 1, 5, 10]
+xi = []
+for j in np.arange(-1, 10, 0.01):
+    xi.append(j)
+
+def mykde(X,h,x):
+    sum_k = 0.0
+    if (dim == 1):   # To check for 1-D or 2-D data
+        N = len(X)   # number of data points in data set
+        for i in range(N):
+            ui = (x - X[i]) / h
+            if (abs(ui) <= 0.5):        # condition to check value within hypercube(h=kernel functio represented as hypercube)
+                k = 1
+            else:
+                k = 0
+            sum_k = sum_k + k           # find summation i.e. total #of data points in cube
+        px = float(sum_k / (N * h))     # estimated density at x
+        return px, x
+    
+    if (dim == 2):
+        N = len(X)
+        for i in range(N):
+            u1 = (x - X[i][0]) / h
+            u2 = (x - X[i][1]) / h
+            if (abs(u1) <= 0.5 and abs(u2) <= 0.5):
+                k = 1
+            else:
+                k = 0
+            sum_k = sum_k + k
+        px = float(sum_k / (N * h * h))
+        return px, x
+
+# Part 1(a) : Generate N = 1000 Gaussian random data with u = 5 and sigma = 1. Test function mykde on data with h=[0.1,1,5,10].
+u = 5
+sigma = 1
+N = 1000
+dim = 1
+X = np.random.normal(u, sigma, N)
+p1 = [0.0] * (len(xi))
+p2 = [0.0] * (len(xi))
+p3 = [0.0] * (len(xi))
+p4 = [0.0] * (len(xi))
+
+for i in range(len(xi)):
+    p1[i], xi[i] = mykde(X, h[0], xi[i])
+    p2[i], xi[i] = mykde(X, h[1], xi[i])
+    p3[i], xi[i] = mykde(X, h[2], xi[i])
+    p4[i], xi[i] = mykde(X, h[3], xi[i])
+
+figr1, axis = plt.subplots(nrows = 5, ncols = 1)
+figr1.canvas.set_window_title('Part 1(A)')
+
+axis[0].hist(X, 100, density = True, color = 'b')
+axis[1].plot(xi, p1, color = 'b')
+axis[2].plot(xi, p2, color = 'b')
+axis[3].plot(xi, p3, color = 'b')
+axis[4].plot(xi, p4, color = 'b')
+
+axis[1].set_title('h=0.1 Part1(a)')
+axis[2].set_title('h=1 Part1(a)')
+axis[3].set_title('h=5 Part1(a)')
+axis[4].set_title('h=10 Part1(a)')
+
+plt.xlabel("x-axis data points(xi)")
+plt.ylabel("y-axis estimated densities(px)")
+
+# Part 1(b) : Generate N = 1000 1-D Gaussian random data with u1 = 5 and sigma1 = 1 and another Gaussian random data 
+#with u2 = 0 and sigma2 = 0.2. Test function mykde on data with h=[0.1,1,5,10].
+u1 = 5
+sigma1 = 1
+u2 = 0
+sigma2 = 0.2
+N1 = 500
+N2 = 500
+dim = 1
+y1 = np.random.normal(u1, sigma1, N1)
+y2 = np.random.normal(u2, sigma2, N2)
+X = np.concatenate((y1, y2), axis = 0)
+p1 = [0.0] * (len(xi))
+p2 = [0.0] * (len(xi))
+p3 = [0.0] * (len(xi))
+p4 = [0.0] * (len(xi))
+
+for i in range(len(xi)):
+    p1[i], xi[i] = mykde(X, h[0], xi[i])
+    p2[i], xi[i] = mykde(X, h[1], xi[i])
+    p3[i], xi[i] = mykde(X, h[2], xi[i])
+    p4[i], xi[i] = mykde(X, h[3], xi[i])
+
+figr2, axis = plt.subplots(nrows = 5, ncols = 1)
+figr2.canvas.set_window_title('Part 1(B)') 
+
+axis[0].hist(X, 100, density = True, color = 'r')
+axis[1].plot(xi, p1, color = 'r')
+axis[2].plot(xi, p2, color = 'r')
+axis[3].plot(xi, p3, color = 'r')
+axis[4].plot(xi, p4, color = 'r')
+
+axis[1].set_title('h=0.1 Part1(b)')
+axis[2].set_title('h=1 Part1(b)')
+axis[3].set_title('h=5 Part1(b)')
+axis[4].set_title('h=10 Part1(b)')
+
+plt.xlabel("x-axis data points(xi)")
+plt.ylabel("y-axis estimated densities(px)")
+
+# Part 2: 2 sets of 2-D Gaussian random data
+
 u1 = [1,0]
-u2 = [0,1.5]
-cov1 = [[0.9,0.4],[0.4,0.9]]
-cov2 = [[0.9,0.4],[0.4,0.9]]
+u2 = [0,2.5]
+sigma1 = [[0.9,0.4],[0.4,0.9]]
+sigma2 = [[0.9,0.4],[0.4,0.9]]
+N = 500
+dim = 2
+y1 = np.random.multivariate_normal(u1, sigma1, N)
+y2 = np.random.multivariate_normal(u2, sigma2, N)
+X = np.concatenate((y1,y2), axis = 0)
+p1 = [0.0] * (len(xi))
+p2 = [0.0] * (len(xi))
+p3 = [0.0] * (len(xi))
+p4 = [0.0] * (len(xi))
 
-# 2-D guassian random data
-x = np.random.multivariate_normal(u1, cov1, N)
-y = np.random.multivariate_normal(u2, cov2, N)
+for i in range(len(xi)):
+    p1[i], xi[i] = mykde(X, h[0], xi[i])
+    p2[i], xi[i] = mykde(X, h[1], xi[i])
+    p3[i], xi[i] = mykde(X, h[2], xi[i])
+    p4[i], xi[i] = mykde(X, h[3], xi[i])
 
-X_set = np.concatenate((x,y), axis = 0)
+figr3, axis = plt.subplots(nrows = 5, ncols = 1)
+figr3.canvas.set_window_title('Part 2') 
 
-K_value = input("Enter the number of clusters: ")
-k = int(K_value)
-c = []
+axis[0].hist(X, 100, density = True)
+axis[1].plot(xi, p1, color = 'y')
+axis[2].plot(xi, p2, color = 'y')
+axis[3].plot(xi, p3, color = 'y')
+axis[4].plot(xi, p4, color = 'y')
 
-for x in range(k):
-    centers_val = input("Enter the centers for cluster "+ str(x+1) +" : ")
-    centers = []
-    for x in centers_val.split(" "):
-        centers.append(float(x))
-    c.append(centers)
-c = np.asarray(c, dtype = np.float64)
+axis[1].set_title('h=0.1 Part 2')
+axis[2].set_title('h=1 Part 2')
+axis[3].set_title('h=5 Part 2')
+axis[4].set_title('h=10 Part 2')
 
-def myKmeans(X,k,c):
-    #features of data
-    #fea = X_set.shape[1]
-    # training data
-    train = X_set.shape[0]
-    #intializing an array with same shape as c
-    c_old = np.zeros(c.shape)
-    #new centers
-    c_new = copy.deepcopy(c)
-    #initializing the clusters to zero
-    clusters = np.zeros(train)
-    #distance of data set from cluster k
-    distance = np.zeros((train,k))    
-    #l2 norm between previous centers and updated centers is <=0.001 or number of iterations = 10000
-    iterate = 0    
-    while (np.linalg.norm(c_new-c_old) > 0.001):
-        iterate = iterate+1
-        print("Iteration: ",iterate)
-        #check for the number of iterations
-        if iterate == 10000:
-            break
+plt.xlabel("x-axis data points(xi)")
+plt.ylabel("y-axis estimated densities(px)")
+plt.show()
 
-        for x in range(k):
-            distance[:, x] = np.linalg.norm(X_set - c_new[x], axis = 1)
-        #assigning the data sets to the closest center
-        clusters = np.argmin(distance, axis = 1)
-        print(clusters)
-        #updating the old centers
-        c_old = copy.deepcopy(c_new)
-        #calculating the new centers by finding the average of data set for a cluster
-        for x in range(k):
-            c_new[x] = np.average(X_set[clusters == x], axis = 0 )
-        
-    print("Centers after kmeans: ", c_new)
-
-    #scatter plot
-    xaxis = []
-    yaxis = []
-    for a in X_set:
-        for d in a:
-            if d == a[0]:
-                xaxis.append(d)
-            if d == a[1]:
-                yaxis.append(d)
-    #print("xaxis :",xaxis)
-    #print("yaxis :",yaxis)
-    # color = [red, blue, green, yellow]
-    for c,x,y in zip(clusters, xaxis, yaxis):
-        if c == 0:
-            plt.scatter(x, y, color = 'b')
-            continue
-        elif c == 1:
-            plt.scatter(x, y, color = 'y')
-            continue
-        elif c == 2:
-            plt.scatter(x, y, color = 'm')
-            continue
-        elif c == 3:
-            plt.scatter(x, y, color = 'g')
-            continue
-
-    for x,y in c_new:
-        plt.scatter(x, y, color = 'r')
-
-    # plt.scatter(xplots, yplots)
-    plt.xlabel("x-axis")
-    plt.ylabel("y-axis")
-    plt.show()
-
-
-myKmeans(X_set,k,c)
--- q2.py
+++ q2.py
@@ -1,150 +1,223 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  2 18:47:22 2020
-
-@author: Shipra
-"""
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
+import scipy.stats as ss
+import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib.pyplot as plt 
+from sklearn.feature_extraction.text import TfidfVectorizer  #library for TFidf weight matrix
 
-with open('Amazon_Reviews.csv', 'r') as f:
-    p = []
-    for line in f:
-            words = line.split(',')
-            p.append((words[0]))
-    print (p)
+u1 = [1,0]
+u2 = [0,1]
+sigma1 = [[1, 0.75],[0.75, 1]]
+sigma2 = [[1, 0.75],[0.75, 1]]
 
-vectorizer = TfidfVectorizer()
-matrix = vectorizer.fit_transform(p)
-print(matrix)
-df1 = pd.DataFrame(matrix.toarray(), columns=vectorizer.get_feature_names()) #dataframes using panda
-print(df1)
+class NaiveBayes:  
+    
+    def data_calc(self, values):
+        label0_train = np.zeros(values).T
+        label1_train = np.ones(values).T
+        labels_trained = np.append(label0_train,label1_train,axis=0)
+        label0_test = np.zeros(500).T
+        label1_test = np.ones(500).T
+        labels_tested = np.append(label0_test,label1_test,axis=0)
+        trainset1 = np.random.multivariate_normal(u1, sigma1, values)
+        trainset2 = np.random.multivariate_normal(u2, sigma2, values)
+        traindata = np.append(trainset1, trainset2, axis=0)
+        testset1 = np.random.multivariate_normal(u1, sigma1, 500)
+        testset2 = np.random.multivariate_normal(u2, sigma2, 500)
+        testdata = np.append(testset1,testset2, axis=0)
+        return (traindata, testdata, labels_trained, labels_tested)
+    
+    def myNB(self,X,Y,X_test,Y_test):
+        df=pd.DataFrame((X),columns=['X','Y'])
+        df['label']=Y        
 
-df_array = df1.to_numpy()
-color_map = plt.imshow(df_array) #weight matrix displayed.
-color_map.set_cmap("jet")
-plt.colorbar()
-plt.show()   #visualization of matrix is displayed
-#part a done count matrix
+        label_0=df[df['label']==0]
+        label_1=df[df['label']==1]
+        
+        prior_0=len(label_0)/(len(label_0)+len(label_1))
+        prior_1=len(label_1)/(len(label_0)+len(label_1))
+        
+        label_0mean = label_0.mean()
+        label_1mean = label_1.mean()
+        label_0std = label_0.std()
+        label_1std = label_1.std()        
 
-vec = CountVectorizer()
-X = vec.fit_transform(p)
-df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names())
-df.to_csv('file1.csv')
-print(df)
+        class0=[]
+        class1=[]
+        df_test=pd.DataFrame((X_test),columns=['X','Y'])
+        for rows in df_test.iterrows():
+            class0.append((ss.norm.pdf(rows[1]['X'],label_0mean['X'],label_0std['X']))*(ss.norm.pdf(rows[1]['Y'],label_0mean['Y'],label_0std['Y']))*prior_0)
+            class1.append((ss.norm.pdf(rows[1]['X'],label_1mean['X'],label_1std['X']))*(ss.norm.pdf(rows[1]['Y'],label_1mean['Y'],label_1std['Y'])*prior_1))
+        df_test['class_0_prob']=class0
+        df_test['class_1_prob']=class1
+        #print (df_test)      
 
-wordsGood = "great,like,good,love,fun"
-wordsGood = wordsGood.split(",")
-wordsBad = "awful,not,dont,only,bad"
-wordsBad = wordsBad.split(",")
-print('Good Words: ', wordsGood)
-print('Bad Words: ',wordsBad)
-words = wordsGood + wordsBad
-print('Total Words:', words)
-#part b good bad done
+        prediction=[]
+        for rows in df_test.iterrows():
+            if rows[1]['class_0_prob']>rows[1]['class_1_prob']:
+                prediction.append(0)
+            else:
+                prediction.append(1)
+                
+        df_test['predict']=prediction
+        
+        df_test['actual']=Y_test        
 
-#part b tdift matrix
-dfCount = df[words]
-dftfidt = df1[words]
-print(dfCount)
-print(dftfidt)
+        count=0
+        
+        for rows in df_test.iterrows():
+            if rows[1]['predict']==rows[1]['actual']:
+                count+=1
+                
+        accuracy=count/(df_test.shape[0])
+        error=1-accuracy
+        tp=0
+        fp=0
+        tn=0
+        fn=0
+        class_0_x=[]
+        class_0_y=[]
+        class_1_x=[]
+        class_1_y=[]
+        for rows in df_test.iterrows():
+            if rows[1]['predict']==rows[1]['actual']:
+                if rows[1]['predict']==1:
+                    tp+=1
+                else:
+                    tn+=1
+            elif rows[1]['predict']==1 and rows[1]['actual']==0:
+                fp+=1
+            else:
+                fn+=1
+            if rows[1]['predict']==0:
+                class_0_x.append(rows[1]['X'])
+                class_0_y.append(rows[1]['Y'])
+            else:
+                class_1_x.append(rows[1]['X'])
+                class_1_y.append(rows[1]['Y'])
+        plt.title('ScatterPlot of labeled Data')
+        plt.scatter(class_0_x,class_0_y,1,'green', label = 'Label 0')        
+        plt.scatter(class_1_x,class_1_y,1,'red' , label = 'Lable 1')
+        plt.show()      
 
-dftfidtGBWordTemp = dftfidt[wordsGood]
-dftfidtGBWordTempBad = dftfidt[wordsBad]
-dftfidtGBWordSum = dftfidtGBWordTemp.sum(axis = 1)
-dftfidtGBWordSumBad = dftfidtGBWordTempBad.sum(axis = 1) 
-Sum = pd.concat([dftfidtGBWordSum,dftfidtGBWordSumBad], axis = 1)
-print('Sum tfidf:',Sum)
-X = Sum.values.tolist()
+        accuracy=((tp+tn)/(tp+tn+fp+fn))*100
+        print('Average Accuracy (%): ',accuracy)
+        
+        error = 100 - accuracy
+        print('Average Error Rate: ' , error)       
 
-#
+        recall=(tp/(tp+fn))*100
+        print('Average Recall:',recall)        
 
-import copy
-#
-X = np.array(X)
-#
-n = X.shape[0]  #number of obejcts
+        precision=(tp/(tp+fp))*100
+        print('Average Precision:',precision)        
 
-k = 2
-c = [[0.15,0.15],[0.3,0.3]]
-
-c = np.array(c)
-c = c.T
-
-count = 0
-
-def myKmeans(X,k,c):
-    #features of data
-    #fea = X_set.shape[1]
-    # training data
-    train = X.shape[0]
-    #intializing an array with same shape as c
-    c_old = np.zeros(c.shape)
-    #new centers
-    c_new = copy.deepcopy(c)
-    #initializing the clusters to zero
-    clusters = np.zeros(train)
-    #distance of data set from cluster k
-    distance = np.zeros((train,k))    
-    #l2 norm between previous centers and updated centers is <=0.001 or number of iterations = 10000
-    iterate = 0    
-    while (np.linalg.norm(c_new-c_old) > 0.001):
-        iterate = iterate+1
-        print("Iteration: ",iterate)
-        #check for the number of iterations
-        if iterate == 10000:
-            break
-
-        for x in range(k):
-            distance[:, x] = np.linalg.norm(X - c_new[x], axis = 1)
-        #assigning the data sets to the closest center
-        clusters = np.argmin(distance, axis = 1)
-        print(clusters)
-        #updating the old centers
-        c_old = copy.deepcopy(c_new)
-        #calculating the new centers by finding the average of data set for a cluster
-        for x in range(k):
-            c_new[x] = np.average(X[clusters == x], axis = 0 )
+        conf_matrix=pd.DataFrame([[tp,fn],[fp,tn]],columns=['Actual 1','Actual 0'])
+        print('Confusion matrix\n:',conf_matrix)   
+        
+        po = pd.DataFrame(class0,columns=['class_0_prob'])
+        po['class_1_prob'] = class1
+        return (prediction,po,error)
+    
+    def part_1_1(self):
+        print('Part 1_1')
+        a,b,c,d= self.data_calc(500)
+        pred,posterior,err= self.myNB(a,c,b,d)
+        frame = pd.DataFrame(posterior, columns=['class_0_prob','class_1_prob'])
+        frame['actual']=d
+        self.calc_ROC_curve(frame)    
+    
+    def part_1_2(self):
+        print('Part 1_2')
+        data_list=[10,20,50,100,300,500]
+        accuracy=[]
+        for i in range (len(data_list)):
+            a,b,c,d=self.data_calc(data_list[i])
+            pred,posterior,err=self.myNB(a,c,b,d)
+            accuracy.append(1-err)
+        plt.title('Changes of accuracies')
+        plt.plot(data_list,accuracy)
+        plt.show()
+    
+    def part_1_3_data_calc(self):
+        label0_train = np.zeros(700).T
+        label1_train = np.ones(300).T
+        labels_trained =np.append(label0_train,label1_train,axis=0)
+        label0_test = np.zeros(500).T
+        label1_test = np.ones(500).T
+        labels_tested =np.append(label0_test,label1_test,axis=0)
+        trainset1 = np.random.multivariate_normal(u1, sigma1, 700)
+        trainset2 = np.random.multivariate_normal(u2, sigma2, 300)
+        traindata = np.append(trainset1, trainset2, axis=0)
+        testset1 = np.random.multivariate_normal(u1, sigma1, 500)
+        testset2 = np.random.multivariate_normal(u2, sigma2, 500)
+        testdata = np.append(testset1,testset2, axis=0)
+        return (traindata, testdata, labels_trained, labels_tested)
+    
+    def part_1_3(self):
+        print('Part 1_3')
+        a,b,c,d= self.part_1_3_data_calc()
+        pred,posterior,err= self.myNB(a,c,b,d)        
+        print(type(posterior))
+        frame = pd.DataFrame(posterior, columns=['class_1_prob'])
+        frame['actual']=d
+        self.calc_ROC_curve(frame)
+        
+    def calc_ROC_curve(self,df_test):
+        print('Part 2')
+        tp=0
+        fp=0
+        tpr=[]
+        fpr=[]
+        actual_p=0
+        actual_n=0
+        fpr_prv=0
+        auc=0
+        df_test=df_test.sort_values(by='class_1_prob', ascending=False)
+        for rows in df_test.iterrows():
+            if rows[1]['actual']==1:
+                actual_p+=1
+            else:
+                actual_n+=1
+        for rows in df_test.iterrows():
+                if rows[1]['actual']==1:
+                    tp+=1
+                else: 
+                    fp+=1
+                tpr.append((tp/(actual_p)))
+                fpr.append((fp/(actual_n)))
+                auc+=((tp/actual_p))*((fp/actual_n)-fpr_prv)
+                fpr_prv=(fp/actual_n)
+        print('Area Under Curve:',auc)
+                
+        plt.title('ROC Curve')
+        plt.plot(fpr,tpr)
+        plt.show()
+    
+    def weightMatrix(self):
+        Amazon_Reviews = pd.read_csv('Amazon_Reviews.csv')  #reading the data file
+        Amazon_Reviews1 = Amazon_Reviews['Review']  #selecting only the review column   it has Y test as labels in the file
+        Review_list = Amazon_Reviews1.values.tolist()  #coverting data to list
+        YTest_amzon = Amazon_Reviews['Label']         
+        YTest_amzon_List = YTest_amzon.values.tolist()  #coverting data to list
+        print(YTest_amzon_List)
+        for i in range(len(YTest_amzon_List)):
+            if YTest_amzon_List[i] == '__label__2 ':
+                YTest_amzon_List[i] = 1
+            else:
+                YTest_amzon_List[i] = 0
+        
+        vectorizer = TfidfVectorizer()
+        matrix = vectorizer.fit_transform(Review_list)
+        df1 = pd.DataFrame(matrix.toarray(), columns=vectorizer.get_feature_names()) #dataframes using panda
+        
+        df_array = df1.to_numpy()
+        print(df_array)
         
 
-    #scatter plot
-    #plt.scatter(X[:,0], X[:,1], c = 'k')   #, lable = 'Unclsuter Data'
-    xaxis = []
-    yaxis = []
-    for a in X:
-        for d in a:
-            if d == a[0]:
-                xaxis.append(d)
-            if d == a[1]:
-                yaxis.append(d)
-    #print("xaxis :",xaxis)
-    #print("yaxis :",yaxis)
-    # color = [red, blue, green, yellow]
-    
-    for c,x,y in zip(clusters, xaxis, yaxis):
-        if c == 0:
-            plt.scatter(x, y, color = 'b')
-            continue
-        elif c == 1:
-            plt.scatter(x, y, color = 'y')
-            continue
-        elif c == 2:
-            plt.scatter(x, y, color = 'm')
-            continue
-        elif c == 3:
-            plt.scatter(x, y, color = 'g')
-            continue
-
-    for x,y in c_new:
-        plt.scatter(x, y, color = 'r')
-
-    # plt.scatter(xplots, yplots)
-    plt.xlabel("x-axis")
-    plt.ylabel("y-axis")
-    plt.show()
-
-clusters = myKmeans(X, k, c)
-print('Number Iterations:', count)
+def main():
+    nb = NaiveBayes()
+    nb.part_1_1()
+    nb.part_1_2()
+    nb.part_1_3()
+    nb.weightMatrix()
+main()
